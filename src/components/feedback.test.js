d95226a5650a073be3c0440f1d97aa0115deb30b
Nicole Bradford <nbradf2@gmail.com>
2018-01-21 11:51:09 -0600

    add render feedback test

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -7,4 +7,11 @@ describe('<Feedback />', () => {
 	it('Renders without crashing', () => {
 		shallow(<Feedback />);
 	});
+
+	it('Renders feedback', () => {
+		let TEST_FEEDBACK = 'You are playing a game.';
+		let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
+			expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+	});
 });
+
