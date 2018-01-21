c2a302fe4aa25bc8f0a6abbb2bd0a6b930fbcfb7
Nicole Bradford <nbradf2@gmail.com>
2018-01-21 12:37:54 -0600

    add smoke test

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,4 +7,14 @@ describe('<GuessList />', () => {
 	it('Should render without crashing', () => {
 		shallow(<GuessList guesses={[]}/>);
 	})
-})
+
+	it('Should render a list of guesses', () => {
+		const values = [10, 25, 50];
+		const wrapper = shallow(<GuessList guesses={values} />);
+		const items = wrapper.find('li');
+		expect(items.length).toEqual(values.length);
+		values.forEach((value, index) => {
+			expect(items.at(index).text()).toEqual(value.toString());
+		});
+	});
+});
