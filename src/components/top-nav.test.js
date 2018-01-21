6994b2db7ccde07ea17a32a450f1c14fa8385d06
Nicole Bradford <nbradf2@gmail.com>
2018-01-21 12:43:39 -0600

    add smoke test

---
+++ src/components/top-nav.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {TopNav} from './top-nav';
+
+describe('<TopNav />', () => {
+	it('Should render without crashing', () => {
+		shallow(<TopNav />);
+	});
+});
